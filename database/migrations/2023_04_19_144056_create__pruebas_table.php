9bafc34a99f56b7ad6c9e98e9b9b4698e78e87a0
Limbernav21 <limbernav17@gmail.com>
2023-06-21 11:26:41 -0600

    Primer commit

---
+++ database/migrations/2023_04_19_144056_create__pruebas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePruebasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('_pruebas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('email')->unique();
+            $table->timestamp('fecha_nacimiento')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('_pruebas');
+    }
+}
